7da2a14e55329ac505dd2ce9e2b0ead50b8bcd24
iiiv <simonbl@outlook.com>
2021-01-13 12:08:05 +0100

    Readded dependency-pom for MQ test folder

--- build_and_run.sh
+++ build_and_run.sh
@@ -19,10 +19,10 @@ popd
 # Update the set of services and
 # build and execute the system tests
 pushd demo_client
+# docker-compose down # We dont take down the containers after tests
 ./deploy.sh 
 sleep 20s
 ./test.sh
-# docker-compose down # We dont take down the containers after tests
 popd
 
 # Cleanup the build images
