522e6343e0bf98cf605ef69dcacd6708c18ec7e6
Martinnes <martin@hemmingsens.dk>
2021-01-13 08:49:40 +0100

    accessing merchant repository through interface

--- DTUPay/src/main/java/merchantservice/MerchantService.java
+++ DTUPay/src/main/java/merchantservice/MerchantService.java
@@ -6,20 +6,21 @@ import java.util.UUID;
 
 public class MerchantService {
     public static MerchantService instance = new MerchantService();
-    List<Merchant> merchants = new ArrayList<>();
+    IMerchantRepository merchantRepository = new MerchantInMemoryRepository();
+
 
     public String registerMerchant(Merchant merchant){
         if(merchant.accountId==null || merchant.accountId.length()==0)
             throw new IllegalArgumentException("Merchant must have an account id to be created in DTUPay");
         merchant.id = String.valueOf(UUID.randomUUID());
-        merchants.add(merchant);
+        merchantRepository.addMerchant(merchant);
         return merchant.id;
     }
 
     public boolean merchantExists(String merchantId){
         if(merchantId==null)
             return false;
-        for(Merchant Merchant : merchants)
+        for(Merchant Merchant : merchantRepository.getAllMerchants())
             if(Merchant.id.equals(merchantId))
                 return true;
         return false;
