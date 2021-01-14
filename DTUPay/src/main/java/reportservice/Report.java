db727f41c161d4f1129ead92a9f9b5a4a17db7e1
Jakob Offersen <jakoboffersen@gmail.com>
2021-01-14 11:59:08 +0100

    tmp commit for successful reporting. Misses server side generation of reports

---
+++ DTUPay/src/main/java/reportservice/Report.java
@@ -0,0 +1,31 @@
+package CustomerMobileApp;
+
+import dtu.ws.fastmoney.User;
+
+import java.util.List;
+
+public class Report {
+
+    private List<Transaction> transactions;
+    private User merchant;
+
+    public Report() {}
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public User getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(User merchant) {
+        this.merchant = merchant;
+    }
+
+
+}
---
+++ DTUPay/src/main/java/reportservice/ReportService.java
@@ -0,0 +1,2 @@
+package reportservice;public class ReportService {
+}
