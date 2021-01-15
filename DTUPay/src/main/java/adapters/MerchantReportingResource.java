3ce89a7455acc5ed1fefd4b3a49996aa4ad965a0
iiiv <simonbl@outlook.com>
2021-01-15 22:33:40 +0100

    selective container building

--- DTUPay/src/main/java/adapters/MerchantReportingResource.java
+++ DTUPay/src/main/java/adapters/MerchantReportingResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Path("/merchantapi/reports")
 public class MerchantReportingResource {
