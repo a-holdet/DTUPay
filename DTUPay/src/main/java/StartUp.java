2689a6dbe487304d852fb40359623da0264b6e5a
s153587 <s153587@student.dtu.dk>
2021-01-16 22:44:01 +0100

    working microservice!

---
+++ DTUPay/src/main/java/StartUp.java
@@ -0,0 +1,10 @@
+package messagequeue;
+
+import messaging.rmq.event.objects.Event;
+
+public class StartUp {
+
+    public static void main(String[] args) throws Exception {
+        EventService.getInstance();
+    }
+}
