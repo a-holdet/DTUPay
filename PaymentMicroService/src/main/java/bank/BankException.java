6790248ac4a5c333cec311d0b3b7f6dc5c28f4f7
Martinnes <martin@hemmingsens.dk>
2021-01-19 16:50:38 +0100

    AUthors external bank and messagequeuebase

--- PaymentMicroService/src/main/java/bank/BankException.java
+++ PaymentMicroService/src/main/java/bank/BankException.java
@@ -1,5 +1,9 @@
 package bank;
 
+/***
+ * @Author Martin Hemmingsen, s141887
+ */
+
 public class BankException extends Exception {
     public BankException(String errorMessage) {
         super(errorMessage);
--- PaymentMicroService/src/main/java/bank/IBank.java
+++ PaymentMicroService/src/main/java/bank/IBank.java
@@ -1,6 +1,10 @@
 package bank;
 
 import java.math.BigDecimal;
+/***
+ * @Author Martin Hemmingsen, s141887
+ */
+
 
 public interface IBank {
     void transferMoneyFromTo(String customerAccountId, String merchantAccountId, BigDecimal amount, String description) throws BankException;
