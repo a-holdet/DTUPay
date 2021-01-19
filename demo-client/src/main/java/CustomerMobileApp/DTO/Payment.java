9f4381603083959de5235e89f87208f986a85ea1
Jakob Offersen <jakoboffersen@gmail.com>
2021-01-19 08:13:35 +0100

    minor

--- demo-client/src/main/java/CustomerMobileApp/DTO/Payment.java
+++ demo-client/src/main/java/CustomerMobileApp/DTO/Payment.java
@@ -4,18 +4,8 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Payment {
-
     public UUID customerToken;
     public String merchantId;
     public String description;
     public BigDecimal amount;
-
-    public Payment() {}
-
-    public Payment(UUID customerToken, String merchantId, String description, BigDecimal amount) {
-        this.customerToken = customerToken;
-        this.merchantId = merchantId;
-        this.description = description;
-        this.amount = amount;
-    }
 }
